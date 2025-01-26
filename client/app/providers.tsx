c2e9f4816afe073fffd4d7cae29d446b1f9ad709
Pratik <93414233+pratik-git10@users.noreply.github.com>
2025-01-26 22:38:53 +0530

    Added Initail Config

---
+++ client/app/providers.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import React, { ReactNode } from "react";
+import StoreProvider from "@/state/redux";
+
+const Providers = ({ children }: { children: ReactNode }) => {
+  return <StoreProvider>{children}</StoreProvider>;
+};
+
+export default Providers;
