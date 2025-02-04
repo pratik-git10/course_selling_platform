bf86d704fdc2e5186328898e08c0962ca7e3ca53
Pratik <93414233+pratik-git10@users.noreply.github.com>
2025-02-05 00:01:41 +0530

    Completed Footer

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -1,9 +1,20 @@
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
   return (
     <div className="bg-customgreys-secondarybg bottom-0 w-full py-8 mt-10 text-center text-sm">
       <p>&copy; 2025 Coursel. All Right reserved</p>
+      <div className="mt-2">
+        {["About", "Privacy Policy", "License", "Contact"].map((item) => (
+          <Link
+            className="text-primary-500 mx-2"
+            key={item}
+            href={`/${item.toLowerCase().replace(" ", "-")}`}>
+            {item}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
